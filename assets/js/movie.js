b948ca521dce75cd65a5add4609e3199ad2c2b3c
carolina-silvag <carolinasguzman@gmail.com>
2018-01-09 18:33:30 -0300

    cosas js

--- assets/js/movie.js
+++ assets/js/movie.js
@@ -1,3 +1,14 @@
-var randomMovieArray = ['Star Wars', 'Game of Thrones', 'Lord of the anillos', 'harry potter'];
+/*var randomMovieArray = ['Star Wars', 'Game of Thrones', 'Lord of the anillos', 'harry potter'];
 
-var randomNumber = math.floor((Math.random))
+var randomNumber = math.floor((Math.random))*/
+
+
+
+$('#btnSearch').click(search);
+function search() {
+  setSearchMovie($('#search').val());
+}
+
+function setSearchMovie(movie) {
+	console.log($.ajax({url : "https://www.omdbapi.com/?apikey=9e7fd663"}))
+}
